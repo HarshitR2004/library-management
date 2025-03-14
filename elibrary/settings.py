c4813987ee441ab6d0e9c29f2213d1064bf71658
HarshitR2004 <harshithranjan6971@gmail.com>
2025-03-14 10:04:19 +0530

    Update settings.py

--- elibrary/settings.py
+++ elibrary/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'books',
+    
 ]
 
 ROOT_URLCONF = 'elibrary.urls'
